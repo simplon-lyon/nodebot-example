683c0c17efdab6fc748d6c7e2f426c0c0ae620c6
Simplon <jdemel@simplon.com>
2017-08-18 14:55:40 +0200

    added http server for bot instructions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Robot = require('./robot');
 const ButtonPress = require('./button-press');
+const http = require('http');
 
 let robby = new Robot();
 
@@ -7,3 +8,15 @@ robby.addModule(new ButtonPress());
 
 robby.start();
 
+
+const server = http.createServer(function(request,response){
+    robby.moveForward();
+    setTimeout(function(){
+        robby.stop();
+    },1000);
+    response.end('instruction prise en compte');
+});
+
+server.listen(8181, function() {
+    console.log('listening on 8181');
+});
